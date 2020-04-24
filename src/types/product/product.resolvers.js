d6298b594bb2ecc58031d4f52e2e6a4e10f5e254
ec-miller <ericmiller@umn.edu>
2020-04-23 19:56:20 -0500

    completed lesson-3 tests

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,10 +9,49 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const products = () => {
+  return Product.find({}).exec()
+}
+
+const product = (_, args) => {
+  return Product.findById(args.id)
+    .lean()
+    .exec()
+}
+
+const newProduct = async (_, args) => {
+  const user = mongoose.Types.ObjectId()
+  return Product.create({ ...args.input, createdBy: user })
+}
+
+const updateProduct = async (_, args) => {
+  return Product.findByIdAndUpdate(args.id, args.input, { new: true })
+    .lean()
+    .exec()
+}
+
+const removeProduct = async (_, args) => {
+  return Product.findByIdAndRemove(args.id)
+    .lean()
+    .exec()
+}
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    products,
+    product
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy(product) {
+      User.findById(product.createdBy)
+        .lean()
+        .exec()
+    }
   }
 }
